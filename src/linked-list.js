f7bcf658903c9cc35a0fe8245cdadf1581a9108f
Kavva <KavvaP17@list.ru>
2017-01-29 20:55:34 +0300

    doubly-linked-list/feature:corrected-errors

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,50 +1,56 @@
 const Node = require('./node');
 
-Function element(value) {
-    this.data = value;
-    this.previous = null;
-    this.next = null;
-}
-
 class LinkedList {
-    
     constructor() 
     {
         this.length = 0;
-        this.head = null;
-        this.tail = null;
+        this._head = null;
+        this._tail = null;
     }
 
     append(data) 
     {
-        var element=new element(data);
+        var element=new Node(data);
         if (this.length>0){
-            this.tail.next = element;
-            element.previous = this.tail;
-            this.tail = element;
+            this._tail.next = element;
+            element.previous = this._tail;
+            this._tail = element;
         }else{
-            this.head=element;
-            this.tail=element;
+            this._head=element;
+            this._tail=element;
         }
         this.length++;
+        return this;
     }
 
     head() 
     {
-        return this.head;
+        if (this.length>0){
+            return this._head.data;    
+        }
+        else{
+            return null;
+        }
+        
     }
 
     tail() 
     {
-        return this.tail;
+        if (this.length>0){
+            return this._tail.data;    
+        }
+        else{
+            return null;
+        }
+        
     }
 
     at(index) 
     {
-        var current = this.head;
-        var pos=1;
-        if (this.length<1 || index<1 || index>this.length){
-            return null;
+        var current = this._head;
+        var pos=0;
+        if (this.length<1 || index<0 || index>this.length){
+            return false;
         }
         else
         {
@@ -52,58 +58,52 @@ class LinkedList {
             {
                 current = current.next;
                 pos++;
-            } 
-            return current;
+            }
+            return current.data;
         }
         
     }
 
     insertAt(index, data) 
     {
-        var element=new element(data);
-        var current = this.head;
-        var pos=1;
-        if (index<1 || index>this.length+1){
+        var element=new Node(data);
+        var current = this._head;
+        var pos=0;
+        if (index<0 || index>this.length){
             return false;
         }
         else{
             //вставка в голову
-            if (index==1){
+            if (index==0){
                 if (this.length==0){
-                    this.head=element;
-                    this.tail=element;        
+                    this._head=element;
+                    this._tail=element;        
                 }
                 else{
-                    this.head=element;
-                    this.head.next=current;
+                    this._head=element;
+                    this._head.next=current;
                     current.previous=element;
                 }
             }
             //вставка в конец
             else if (index==this.length){
-                current=this.tail;
-                this.tail=element;
-                this.tail.previous=current;
-                current.next=this.tail;
-            }
-            //вставка в конец+1
-            else if (index==this.length+1){
                 append(data);
             }
+            //вставка между
             else{
                 while(pos<index){
                     current=current.next;
                     pos++;
                 }
-                var before=curent;
-                var after=current.next;
+                before=current.previous;
+                after=current;
                 before.next=element;
-                element.prprevious=before;
-                after.prprevious=element;
+                element.previous=before;
+                after.previous=element;
                 element.next=after;
             }
             this.length++;
-            return true;
+            return this;
         }
     }
 
@@ -122,33 +122,33 @@ class LinkedList {
     clear()
     {
         this.length = 0;
-        this.head = null;
-        this.tail = null;
+        this._head = null;
+        this._tail = null;
     }
 
     deleteAt(index) 
     {
-        var current = this.head;
-        var pos=1;
-        if (this.length<1 || index<1 || index>this.length){
-            return false;
+        var current = this._head;
+        var pos=0;
+        if (this.length<1 || index<0 || index>this.length){
+            return this;
         }
         else{
             //проверка или не голова
-            if (index==1){
-                this.head=this.head.next;
+            if (index==0){
+                this._head=this._head.next;
                 //если был только 1 элемент
-                if (!this.head){
-                    this.head.previous=null;
+                if (!this._head){
+                    this._head.previous=null;
                 }
                 else{
-                    this.tail=null;
+                    this._tail=null;
                 }
             }
             //проверка или не хвост
             if (index==this.length){
-                this.tail=this.tail.previous;
-                this.tail.next = null;
+                this._tail=this._tail.previous;
+                this._tail.next = null;
             }
             //где-то между головой и хвостом
             else{
@@ -156,27 +156,37 @@ class LinkedList {
                     current=current.next;
                     pos++;
                 }
-                var before=curent.previous;
+                var before=current.previous;
                 var after=current.next;
                 before.next=after;
                 after.previous=before;
                 current=null;
             }
             this.length--;
+            return this;
         }
     }
 
     reverse() 
     {
-        var current = this.head;
-        this.head=this.tail;
-        this.tail=current;
+        var current = this._head;
+        while(current) {
+            var tmp = current.next;
+            current.next=current.previous;
+            current.previous = tmp;
+            current =tmp;
+        }
+        var tmp = this._head;
+        this._head = this._tail;
+        this._tail = tmp;    
+
+        return this;
     }
 
     indexOf(data) 
     {
-        var current = this.head;
-        var pos=1;
+        var current = this._head;
+        var pos=0;
         while (pos < this.length) 
         {
             if (current.data==data){
@@ -185,7 +195,7 @@ class LinkedList {
             current = current.next;
             pos++;
         } 
-        return false;
+        return -1;
     }
 }
 
